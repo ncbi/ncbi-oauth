7ee1301aaecc4a30f0346007332d560aa8e83f45
ksrodarmer <rodarme1@ncbi.nlm.nih.gov>
2018-10-30 11:39:55 -0400

    pulled printing code into functions for jwt_ut.cpp

--- ncbi-oauth-test/ncbi-jwt-ut.cpp
+++ ncbi-oauth-test/ncbi-jwt-ut.cpp
@@ -20,12 +20,58 @@ namespace ncbi
     class JWTFixture_BasicConstruction : public :: testing :: Test
     {
     public:
+        void printClaims ( JWTClaims claims, bool recovered = false )
+        {
+            if ( ! recovered )
+            {
+                std :: cout
+                << "---- JSON Claims ----\n"
+                << "    "
+                << claims . toJSON ()
+                << "\n---- JSON Claims ----\n"
+                << std :: endl
+                ;
+            }
+            else
+            {
+                std :: cout
+                << "---- Decoded Claims ----\n"
+                << "    "
+                << claims . toJSON ()
+                << "\n---- Decoded Claims ----"
+                << std :: endl
+                ;
+            }
+        }
+        
+        void printJWT ( JWT jwt )
+        {
+            std :: cout
+            << "---- JWT ----\n"
+            << "    "
+            << jwt
+            << "\n---- JWT ----\n"
+            << std :: endl
+            ;
+        }
+        
+        void printJWTTransitionStack ( JWTClaims json, JWT jwt, JWTClaims decoded )
+        {
+            std :: cout << "{" << std :: endl;
+            printClaims ( json );
+            printJWT ( jwt );
+            printClaims ( decoded, true );
+            std :: cout << "}\n" << std :: endl;
+        }
+
         void SetUp ()
         {
             jws_fact = new JWSFactory ( "ncbi", "HS384", "blarky2", "key-id-1234", "blarky2" );
             jwt_fact = new JWTFactory ( * jws_fact );
-            jwt_fact -> setIssuer ( "ncbi" );
-            jwt_fact -> setDuration ( 15 );
+            ASSERT_NO_THROW ( jwt_fact -> setIssuer ( "ncbi" ) );
+            ASSERT_NO_THROW ( jwt_fact -> setSubject ( "john doe" ) );
+            ASSERT_NO_THROW ( jwt_fact -> setDuration ( 15 ) );
+            //ASSERT_NO_THROW ( jwt_fact -> setNotBefore ( 60 ) );
         }
         
         void TearDown ()
@@ -39,33 +85,18 @@ namespace ncbi
         JWSFactory * jws_fact;
         JWTFactory * jwt_fact;
     };
+    
    
-    TEST_F ( JWTFixture_BasicConstruction, JWT_Hello )
+    TEST_F ( JWTFixture_BasicConstruction, JWT_Example )
     {
         JWTClaims claims = jwt_fact -> make ();
-        claims . addClaimOrDeleteValue ( "hello", JSONValue :: makeString ( "there" ) );
-        std :: cout
-            << "hello test:\n"
-            << "  JSON:\n"
-            << "    "
-            << claims . toJSON ()
-            << '\n'
-            ;
+        claims . addClaimOrDeleteValue ( "example", JSONValue :: makeString ( "hello there" ) );
+        
         JWT jwt = jwt_fact -> sign ( claims );
-        std :: cout
-            << "  JWT:\n"
-            << "    "
-            << jwt
-            << '\n'
-            ;
 
-        JWTClaims recovered = jwt_fact -> decode ( jwt );
-        std :: cout
-            << "Recovered claims:\n"
-            << "  "
-            << recovered . toJSON ()
-            << std :: endl
-            ;
+        JWTClaims decoded = jwt_fact -> decode ( jwt );
+        
+        printJWTTransitionStack ( claims, jwt, decoded );
     }
 
 } // namespace
